bd91043f840b0a41e66a2ec8a914247a9ff38f7a
GabrielaPSantos <gabrielaperessantos1@gmail.com>
2025-07-11 22:18:59 -0300

    criado aula 16

---
+++ curso-em-video/aula16/funcao02.js
@@ -0,0 +1,8 @@
+function soma(n1=0, n2=0) {
+    return n1 + n2
+}
+
+console.log(soma(2, 5))  
+/* se colocar apenas um valor o n2 fica undefined e aparece na tela NaN 
+por isso usa parâmetro já pré-definido, opcional n=0 
+*/
---
+++ curso-em-video/aula16/funcao03.js
@@ -0,0 +1,5 @@
+let v = function(x) {
+    return x*2
+} // variavel recebe uma função
+
+console.log(v(5))
